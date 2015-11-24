6396512792f2895e8732b316c243d39f54577c36
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-11-24 21:06:40 +0100

    Creades bases de dades. Esperant poder comparar la creació de taules per a que funcioni bé.

---
+++ database/migrations/2015_11_24_194735_create_tasks_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('taks', function(){
+            $table->increments('id');
+            $table->increments('name');
+            $table->increments('id');
+            $table->increments('id');
+            $table->increments('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,30 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UserTableSeeder::class);
+        $faker = Faker/Factory::create();
+        $this->seedTasks($faker);
+        $this->seedTags($faker);
 
         Model::reguard();
+    }
+
+    public function seedTasks($faker)
+    {
+        foreach (range(0_100) as $number){
+        $task = new Task();
+        $task->name = $faker->sentence();
+        $task->done = $faker->boolean();
+        $task->priority = $faker->randomDigit();
+        $task->save;
+        };
+    }
+
+    public function seedTags($faker)
+    {
+        foreach (range(0_100) as $number){
+        $task = new Task()
+        $task->name = $faker->word();
+        $task->save;
+        };
     }
 }
